d48e2370fa4702898c5f0d9c0549ed181ca6ed1e
Donovan <donovanpolk@gmail.com>
2022-06-23 16:58:29 -0400

    First push of full content.

---
+++ index.js
@@ -0,0 +1,12 @@
+let currentProjectTitle = document.getElementById("hero-main-projects-card-h3");
+let currentProjectImage = document.getElementById("hero-main-projects-card-img");
+let currentProjectDesc = document.getElementById("hero-main-projects-card-paragraph");
+
+/* Current Project Card Contents */
+currentProjectTitle.textContent = "Github.io Personal Page";
+currentProjectDesc.textContent = "A personal page for Donovan Polk."
+currentProjectImage.src = "./resources/CurrentProj.png";
+
+viewProjects = () => {
+
+}
